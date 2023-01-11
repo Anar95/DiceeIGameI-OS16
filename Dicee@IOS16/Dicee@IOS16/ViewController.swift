fbd3c15f620d3b2894b285ec831fec3132a33724
Anar95 <anarabbas458@gmail.com>
2023-01-11 22:07:55 +0300

    Update 2

--- Dicee@IOS16/Dicee@IOS16/ViewController.swift
+++ Dicee@IOS16/Dicee@IOS16/ViewController.swift
@@ -15,7 +15,16 @@ class ViewController: UIViewController {
         case Gamer1
         case Gamer2
     }
-
+    
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    
     var firstTurn = Turn.Gamer1
     var currentTurn = Turn.Gamer1
     
@@ -27,30 +36,65 @@ class ViewController: UIViewController {
     var gamer2Score = 0
     
     
-    @IBOutlet weak var imageView1: UIImageView!
-    
-    @IBOutlet weak var imageView2: UIImageView!
-    
-    
-   
     
     @IBAction func changeImage(_ sender: UIButton) {
         
         
         let leftNumber = Int.random(in: 1...5)
         let rightNumber = Int.random(in: 1...5)
-
-   
+        
+        
         imageView1.image = UIImage(named: "Dice\(leftNumber)")
-                                    
+        
         imageView2.image = UIImage(named: "Dice\(rightNumber)")
-
+        
         
     }
     
-   //func checkForVictory(_ s :String) -> Bool{
+    
+    
+    
+    
+    func resetBoard()
+    {
+        for button in board
+        {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if firstTurn == Turn.Gamer2
+        {
+            firstTurn = Turn.Gamer1
+            turnLabel.text = Gamer1
+        }
+        else if firstTurn == Turn.Gamer1
+        {
+            firstTurn = Turn.Gamer2
+            turnLabel.text = Gamer2
+        }
+        currentTurn = firstTurn
+    }
+    
+    func addToBoard(_ sender: UIButton)
+    {
+        if(sender.title(for: .normal) == nil)
+        {
+            if(currentTurn == Turn.Gamer2)
+            {
+                sender.setTitle(Gamer2, for: .normal)
+                currentTurn = Turn.Gamer1
+                turnLabel.text = Gamer1
+            }
+            else if(currentTurn == Turn.Gamer1 )
+            {
+                sender.setTitle(Gamer1, for: .normal)
+                currentTurn = Turn.Gamer2
+                turnLabel.text = Gamer2
+            }
+            sender.isEnabled = false
+        }
         
-   }
-
-
-
+        
+    }
+    
+}
